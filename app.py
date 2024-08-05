2d0950cd84bbb6b99ed6b34a8601dd75768a42c0
Nathan <52205976+nathanzan0310@users.noreply.github.com>
2024-08-05 16:41:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import logging
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from urllib.parse import unquote
 
 
 env_path = Path('.') / '.env'
@@ -27,31 +28,50 @@ def hello():
 def welcome():
     return 'This is the Tencent Overseas Midserver for communication from WeCom to Servicenow. Please contact @nathanan for more information.'
 
-@app.route('/callback', methods=['POST'])
+
+@app.route('/callback', methods=['POST', 'GET'])
 def handle_message():
     msg_signature = request.args.get('msg_signature')
     timestamp = request.args.get('timestamp')
+    # timestamp = '1722375628'
     nonce = request.args.get('nonce')
+    # nonce = 'n6dy7shbz19'
+    echostr = request.args.get('echostr')
     data = request.data
 
-    # Log incoming request
-    logging.info("Received POST request with msg_signature=%s, timestamp=%s, nonce=%s, data=%s",
+    if request.method == 'GET':
+        # Log incoming request
+        logging.info("Received GET request with msg_signature=%s, timestamp=%s, nonce=%s, echostr=%s",
+                 msg_signature, timestamp, nonce, echostr)
+        
+        # Verify URL
+        ret, message = wx_crypt.VerifyURL(msg_signature, timestamp, nonce, echostr)
+
+        if ret != 0:
+            logging.error("Failed to verify URL: %s", ret)
+            return f'error: {ret}', 400
+        
+        return message, 200
+
+    elif request.method == 'POST':
+        # Log incoming request
+        logging.info("Received POST request with msg_signature=%s, timestamp=%s, nonce=%s, data=%s",
                  msg_signature, timestamp, nonce, data)
 
-    # Decrypt message
-    ret, message = wx_crypt.DecryptMsg(data, msg_signature, timestamp, nonce)
+        # Decrypt message
+        ret, message = wx_crypt.DecryptMsg(data, msg_signature, timestamp, nonce)
 
-    if ret != 0:
-        logging.error("Failed to decrypt message: %s", ret)
-        return f'error: {ret}', 400
-    
-    decrypted_message = message.decode('utf-8')  # Decode the decrypted message
-    logging.info("Decrypted message: %s", decrypted_message)
-    print("Decrypted message:", decrypted_message)
-    
-    # Return decrypted message as response for verification (test)
-    return f'decrypted_message: {decrypted_message}', 200
-    return '', 200
+        if ret != 0:
+            logging.error("Failed to decrypt message: %s", ret)
+            return f'error: {ret}', 400
+        
+        # decrypted_message = message.decode('utf-8')  # Decode the decrypted message
+        # logging.info("Decrypted message: %s", decrypted_message)
+        # print("Decrypted message:", decrypted_message)
+        
+        # return f'decrypted_message: {decrypted_message}', 200
+        return '', 200
+    return 'what you doing', 405
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
